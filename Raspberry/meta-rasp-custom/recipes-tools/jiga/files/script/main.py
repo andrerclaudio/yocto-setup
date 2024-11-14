441c70dd0d29850386b5c8bcde38b9f3f81b1d70
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2024-11-14 15:57:57 -0300

    Minor adjuts.

--- Raspberry/meta-rasp-custom/recipes-tools/jiga/files/script/main.py
+++ Raspberry/meta-rasp-custom/recipes-tools/jiga/files/script/main.py
@@ -1,31 +1,6 @@
 #!/usr/bin/env python3
-import serial
 import time
 
-
-time.sleep(60)
-
-# Set up the serial connection (make sure the port and baudrate match your configuration)
-ser = serial.Serial('/dev/ttyAMA0', 57600, timeout=1)
-
-# Check if the serial port is open
-if ser.is_open:
-    print("Serial port opened successfully!")
-else:
-    print("Failed to open serial port.")
-
-try:
-    while True:
-        # Send "Hello" message
-        ser.write(b'Hello\n\r')
-        print("Sent: Hello")
-        # Wait for 1 second
-        time.sleep(0.5)
-
-except KeyboardInterrupt:
-    print("Stopped by user.")
-
-finally:
-    # Close the serial port
-    ser.close()
-    print("Serial port closed.")
+while True:
+    time.sleep(1)
+    # Do nothing
